fd273e82e7ce7abd21aa57ebba9152187e18a5d7
leaisrevolution <leaisrevolution@gmail.com>
2021-12-01 21:18:09 +0900

    11 헤더와 드롭다운 메뉴 - 전역 배지

--- js/main.js
+++ js/main.js
@@ -15,4 +15,11 @@ searchInputEl.addEventListener('blur', function() {
     searchInputEl.setAttribute('placeholder', '');
 });
 
+const badgeEl = document.querySelector('geader .badges');
+
+// window는 출력되고 있는 화면 자체
+window.addEventListener('scroll', function() {
+    console.log('scroll!');
+});
+
 
