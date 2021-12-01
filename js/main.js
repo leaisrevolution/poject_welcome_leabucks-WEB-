895debf1ee1e4fd6ffbe5a04e8b98696de905c10
leaisrevolution <leaisrevolution@gmail.com>
2021-12-01 22:28:33 +0900

    12 헤더와 드롭다운 메뉴 - 전역 배지

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+
 const searchEl = document.querySelector('.search');
 const searchInputEl = searchEl.querySelector('input');
 
@@ -15,11 +16,63 @@ searchInputEl.addEventListener('blur', function() {
     searchInputEl.setAttribute('placeholder', '');
 });
 
-const badgeEl = document.querySelector('geader .badges');
+// const badgeEl = document.querySelector('geader .badges');
+// // window는 출력되고 있는 화면 자체
+// // _.throttle (함수, 시간)
+// // 300 === 0.3초를 의미
+// window.addEventListener('scroll', _.throttle(function() {
+//     console.log(window.scrollY);
+//     if (window.scrollY > 500) {
+//         //배지 숨기기
+//         //gsap.to(요소, 지속시간, 오션)
+//         gsap.to(badgeEl, .6, {
+//             opacity: 0
+//         });
+//     } else {
+//         //배지 보이기
 
-// window는 출력되고 있는 화면 자체
-window.addEventListener('scroll', function() {
-    console.log('scroll!');
-});
+//     }
+// }, 300));
+
+
+const badgeEl = document.querySelector('header .badges')
+const toTopEl = document.querySelector('#to-top')
+// 페이지에 스크롤 이벤트를 추가!
+// 스크롤이 지나치게 자주 발생하는 것을 조절(throttle, 일부러 부하를 줌)
+window.addEventListener('scroll', _.throttle(function () {
+  // 페이지 스크롤 위치가 500px이 넘으면.
+    if (window.scrollY > 500) {
+    // Badge 요소 숨기기!
+    gsap.to(badgeEl, .6, {
+    opacity: 0,
+    display: 'none'
+    })
+    // 상단으로 스크롤 버튼 보이기!
+    gsap.to(toTopEl, .2, {
+        x: 0
+    })
+
+  // 페이지 스크롤 위치가 500px이 넘지 않으면.
+    } else {
+    // Badge 요소 보이기!
+    gsap.to(badgeEl, .6, {
+    opacity: 1,
+    display: 'block'
+    })
+    // 상단으로 스크롤 버튼 숨기기!
+    gsap.to(toTopEl, .2, {
+    x: 100
+    })
+}
+}, 300))
+// 상단으로 스크롤 버튼을 클릭하면,
+toTopEl.addEventListener('click', function () {
+  // 페이지 위치를 최상단으로 부드럽게(0.7초 동안) 이동.
+    gsap.to(window, .7, {
+    scrollTo: 0
+    })
+})
+
+
 
 
